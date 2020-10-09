ab2912c48bfb83d79d8b94fba00871b2ccbd6d2c
hongyiliu <wenhualiu.liu@gmail.com>
2020-10-10 01:32:18 +0800

    first commit

---
+++ my-project1/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e681681fbc9e16921f1a32ab343ff762'),
+    },
+  },
+});
---
+++ my-project1/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8043d125-ed2a-4466-965c-4117edb88965'
+};
